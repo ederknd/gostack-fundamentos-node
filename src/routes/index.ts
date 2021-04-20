9c0adcb4b4708eb1cc43e3ef16a482a7b20df408
ederknd <ederknd@yahoo.com.br>
2021-04-20 20:18:38 -0300

    testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,5 @@ import transactionRouter from './transaction.routes';
 const routes = Router();
 
 routes.use('/transactions', transactionRouter);
-
+routes.use('/users', user);
 export default routes;
